9e81afd53ada8e8e139625296e945c1f54fcfeec
Tim Flood <tflood9@gmail.com>
2021-04-27 15:40:40 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ErrorCatcher extends React.Component {
+  state = { error: null };
+
+  componentDidCatch(error, info) {
+    console.log("[componentDidCatch]", error);
+    this.setState({ error: info.componentStack });
+  }
+
+  render() {
+    if (this.state.error) {
+      return <div>An error occured: {this.state.error}</div>;
+    }
+    return this.props.children;
+  }
 }
 
-export default App;
+class LifecycleDemo extends React.Component {
+  state = { counter: 0 };
+
+  constructor(props) {
+    super(props);
+    console.log("[constructor]");
+    console.log(" State already set:", this.state);
+  }
+
+  componentDidMount() {
+    console.log("[componentDidMount]", "Mounted.");
+  }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("[getDerivedStateFromProps]");
+    console.log(" Next props:", nextProps);
+    console.log(" Prev state:", prevState);
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[shouldComponentUpdate]", "Deciding to update");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(nextProps, nextState) {
+    console.log("[getSnapshotBeforeUpdate]", "About to update...");
+    return `Time is ${Date.now()}`;
+  }
+  // Called after render() and after updating the DOM. The whole
+  // render/commit/update cycle is done.
+  //
+  // This is a good time to check if a prop has changed,
+  // by checking prevProps.whatever === this.props.whatever.
+  // Useful for re-fetching data when a record ID changes.
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[componentDidUpdate]", "Updated.");
+    console.log(" snapshot:", snapshot);
+  }
+  // Called right before the component is unmounted
+  // Time to clean up! Remove any event listeners, cancel
+  // timers, etc.
+  componentWillUnmount() {
+    console.log("[componentWillUnmount]", "Goodbye cruel world.");
+  }
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+  causeErrorNextRender = () => {
+    // Set a flag to cause an error on the next render
+    // This will cause componentDidCatch to run in the parent
+    this.setState({
+      causeError: true,
+    });
+  };
+  render() {
+    console.log("[render]");
+    if (this.state.causeError) {
+      throw new Error("oh no!!");
+    }
+    return (
+      <div>
+        <span>Counter: {this.state.counter}</span>
+        <button onClick={this.handleClick}>Click to increment</button>
+        <button onClick={this.causeErrorNextRender}>Throw an error</button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(
+  <ErrorCatcher>
+    <LifecycleDemo />
+  </ErrorCatcher>,
+  document.querySelector("#root")
+);
